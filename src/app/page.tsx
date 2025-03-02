d443bf8f9e4abc62183a11a5ad0fb9b479f1846b
sandhya6465 <sandhyakumari20082003@gmail.com>
2025-03-02 18:40:49 +0530

    first commitssssasas

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 "use client"; // Enable hooks in Next.js
 import { useEffect, useState } from "react";
-import MemeCard from "./components/MemeCard";
+
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 
@@ -20,11 +20,7 @@ export default function Home() {
       <div className="bg-black text-white min-h-screen">
         <Navbar />
         <h1 className="text-center text-3xl font-bold mt-4">Trending Memes</h1>
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 p-4">
-        {memes.map((meme) => (
-          <MemeCard key={meme.id} meme={meme} />
-        ))}
-      </div>
+        
       </div>
       <Footer />
     </div>
